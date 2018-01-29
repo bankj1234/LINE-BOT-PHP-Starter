2d485f9ab337cf5a25acea9819c5648b1974c97d
chayakorn <bankj123@gmail.com>
2018-01-29 12:03:57 +0700

    no message

--- web/bot.php
+++ web/bot.php
@@ -53,7 +53,7 @@ if (strpos($_msg, 'สอนบอท') !== false) {
                 $arrPostData = array();
                 $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
                 $arrPostData['messages'][0]['type'] = "text";
-                $arrPostData['messages'][0]['text'] = $rec->answer;
+                $arrPostData['messages'][0]['text'] = $isData;
             }
         }else{
             foreach($data as $rec){
